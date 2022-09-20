e296bdbd820ba083aef4d1f7bb36fdb43c748fa8
jjmmkk9 <abcdqwerty0192@gmail.com>
2022-09-20 09:52:47 +0900

    spring boot 시작

---
+++ src/main/java/com/boot/mvc20220916mungi/Mvc20220916MungiApplication.java
@@ -0,0 +1,13 @@
+package com.boot.mvc20220916mungi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvc20220916MungiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvc20220916MungiApplication.class, args);
+	}
+
+}
